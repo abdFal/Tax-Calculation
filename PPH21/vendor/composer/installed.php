348f328593c8ee477493bf7728d68f762a5b3a49
Abdillah Naufal Lesmana <naufaldec07@gmail.com>
2023-02-08 08:38:16 +0700

    Update

--- PPH21/vendor/composer/installed.php
+++ PPH21/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'abdfal/my-php-library',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '646e3210d4b9f5ff8192eed2cb6c3130dee4060d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'abdfal/my-php-library' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '646e3210d4b9f5ff8192eed2cb6c3130dee4060d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
